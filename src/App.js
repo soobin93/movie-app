d13d131149dabc1bf8c62f951032fec331c38108
Soobin <ssbin93@gmail.com>
2020-11-26 23:31:20 +1000

    Learning React Component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const FOODS = [
-  {
-    id: 1,
-    name: "Kimchi",
-    rates: 5
-  }, {
-    id: 2,
-    name: "Ramen",
-    rates: 3
-  }, {
-    id: 3,
-    name: "Tuna"
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
   }
-];
 
-function Food({ name, rates}) {
-  const formattedRates = rates
-    ? `${rates} / 5`
-    : "No rates given";
+  render() {
+    const { isLoading } = this.state;
 
-  return <div>
-    <h3>{ name }</h3>
-    <p>Rates: { formattedRates }</p>
-  </div>;
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rates: PropTypes.number
-}
-
-function App() {
-  return (
-    <div>
-      {FOODS.map(food => {
-        return <Food key={food.id} name={food.name} rates={food.rates} />;
-      })}
-    </div>
-  );
+    return (
+    <div>{isLoading ? "Loading" : "We are ready"}</div>
+    );
+  }
 }
 
 export default App;
