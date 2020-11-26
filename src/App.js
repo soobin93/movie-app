5182f0affce685713752a4125ed1704c7e3be9fe
Soobin <ssbin93@gmail.com>
2020-11-26 14:11:09 +1000

    Add Prop Types Package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from "react";
-import Potato from "./Potato";
+import PropTypes from "prop-types";
+
+const FOODS = [
+  {
+    id: 1,
+    name: "Kimchi",
+    rates: 5
+  }, {
+    id: 2,
+    name: "Ramen",
+    rates: 3
+  }, {
+    id: 3,
+    name: "Tuna"
+  }
+];
+
+function Food({ name, rates}) {
+  const formattedRates = rates
+    ? `${rates} / 5`
+    : "No rates given";
+
+  return <div>
+    <h3>{ name }</h3>
+    <p>Rates: { formattedRates }</p>
+  </div>;
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rates: PropTypes.number
+}
 
 function App() {
   return (
     <div>
-       <h1>Hello World</h1>
-       <Potato />
+      {FOODS.map(food => {
+        return <Food key={food.id} name={food.name} rates={food.rates} />;
+      })}
     </div>
   );
 }
